687a1e533841ee77d21ad91c4026791951c9283f
fokudevy <mileyray512@gmail.com>
2024-12-25 09:43:24 +0100

    updated cluster version to 1.31

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,8 +1,8 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
-    version = "~> 20.0"
+    version = "~> 20.31"
     cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
+    cluster_version = "1.31"
 
     cluster_endpoint_public_access  = true
 
